762c03bfded83a5c15d0ff6737451af8be306a34
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-12-14 21:48:32 -0300

    Lita de itens

--- app/src/main/java/com/leticia/agenda/MainActivity.java
+++ app/src/main/java/com/leticia/agenda/MainActivity.java
@@ -2,8 +2,14 @@ package com.leticia.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +17,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Alex", "Fran", "Leticia", "Maria", "Ana"));
+
+        ListView listaAlunos = findViewById(R.id.activity_main_lista_alunos);
+        listaAlunos.setAdapter(
+                new ArrayAdapter<>(
+                        this, android.R.layout.simple_list_item_1, alunos)
+        );
     }
 }
