638497a5dd3aa90c46bc36b58465077a8f1129f9
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-12-21 22:54:00 -0300

    TENTATIVA FAB

--- app/src/main/java/com/leticia/agenda/MainActivity.java
+++ app/src/main/java/com/leticia/agenda/MainActivity.java
@@ -1,22 +1,22 @@
 package com.leticia.agenda;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de alunos");
         List<String> alunos = new ArrayList<>(
                 Arrays.asList("Alex", "Fran", "Leticia", "Maria", "Ana"));
 
